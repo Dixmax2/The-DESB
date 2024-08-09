7f25fad6c548527f1c4fe46b6b6fc7a14ab5a2ce
julio <juliobielsamunoz@gmail.com>
2024-08-10 01:09:22 +0200

    arreglos basicos

--- thedesbreact/src/componentes/Navbar.js
+++ thedesbreact/src/componentes/Navbar.js
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { NavDropdown, Nav } from 'react-bootstrap';
+import { NavDropdown, Nav, Offcanvas, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import img from '../assets/images/DESBtrans.png';
 import { faUser, faMagnifyingGlass, faCartShopping, faUserSecret, faUserCog } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
 import Modales from './Modales';
 import '../css/navbar.css';
 import { useAppContext } from '../contexto/UserContext';
@@ -16,12 +15,16 @@ const Navbar = () => {
 
   const [showCarro, setShowCarro] = useState(false);
   const [showLupa, setShowLupa] = useState(false);
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
   const handleCloseCarro = () => setShowCarro(false);
   const handleShowCarro = () => setShowCarro(true);
 
   const handleCloseLupa = () => setShowLupa(false);
   const handleShowLupa = () => setShowLupa(true);
+
+  const handleCloseOffcanvas = () => setShowOffcanvas(false);
+  const handleShowOffcanvas = () => setShowOffcanvas(true);
 
   const navigate = useNavigate();
 
@@ -34,92 +37,133 @@ const Navbar = () => {
     dispatch({ type: 'SET_NAME', value: '' });
     navigate('/');
   };
-console.log(name)
-console.log(role)
-console.log(state)
 
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <Modales
-          showCarro={showCarro}
-          handleCloseCarro={handleCloseCarro}
-          showLupa={showLupa}
-          handleCloseLupa={handleCloseLupa}
-        />
-        <a className="navbar-brand" href="/">
-          <img src={img} height="75" width="150" alt="DESB" />
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav navbar-nav-center">
-            <li className="nav-item">
-              <Nav.Link href="#action2"><b>Productos</b></Nav.Link>
-            </li>
-            <li className="nav-item">
-              <Nav.Link href="/vender"><b>Vender o Intercambiar</b></Nav.Link>
-            </li>
-            <li className="nav-item">
-              <NavDropdown
-                title={<span><b>Compañía</b></span>}
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="/nosotros">Nosotros</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="/contact">Contáctanos</NavDropdown.Item>
-              </NavDropdown>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Nav.Link title="Lupa" onClick={handleShowLupa}>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </Nav.Link>
-            </li>
-            {!name && (
+    <>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Modales
+            showCarro={showCarro}
+            handleCloseCarro={handleCloseCarro}
+            showLupa={showLupa}
+            handleCloseLupa={handleCloseLupa}
+          />
+          <a className="navbar-brand" href="/">
+            <img src={img} height="75" width="150" alt="DESB" />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav navbar-nav-center">
               <li className="nav-item">
-                <Nav.Link title="User Anónimo" href="/login">
-                  <FontAwesomeIcon icon={faUserSecret} />
+                <Nav.Link onClick={handleShowOffcanvas}><b>Productos</b></Nav.Link>
+              </li>
+              <li className="nav-item">
+                <Nav.Link href="/vender"><b>Vender o Intercambiar</b></Nav.Link>
+              </li>
+              <li className="nav-item">
+                <NavDropdown
+                  title={<span><b>Compañía</b></span>}
+                  id="navbarScrollingDropdown"
+                >
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/nosotros">Nosotros</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/contact">Contáctanos</NavDropdown.Item>
+                </NavDropdown>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Nav.Link title="Lupa" onClick={handleShowLupa}>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </Nav.Link>
               </li>
-            )}
-            { name && role === "" && (
-              <NavDropdown 
-                title={<span style={{ display: 'flex', alignItems: 'center', color:'white'}}>
-                    <FontAwesomeIcon icon={faUser} className="fa-icon" /> {name}
-                  </span>
-                }
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Divider style={{ background:'white' }} />
-                <NavDropdown.Item onClick={desconectarse} style={{ color:'white' }}>Desconectarse</NavDropdown.Item>
-              </NavDropdown>
-            )}
+              {!name && (
+                <li className="nav-item">
+                  <Nav.Link title="User Anónimo" href="/login">
+                    <FontAwesomeIcon icon={faUserSecret} />
+                  </Nav.Link>
+                </li>
+              )}
+              { name && role === "" && (
+                <NavDropdown 
+                  title={<span style={{ display: 'flex', alignItems: 'center', color:'white'}}>
+                      <FontAwesomeIcon icon={faUser} className="fa-icon" /> {name}
+                    </span>
+                  }
+                  id="navbarScrollingDropdown"
+                >
+                  <NavDropdown.Divider style={{ background:'white' }} />
+                  <NavDropdown.Item onClick={desconectarse} style={{ color:'white' }}>Desconectarse</NavDropdown.Item>
+                </NavDropdown>
+              )}
               {name && role === "admin"  &&  (
-              <NavDropdown 
-                title={
-                  <span style={{ color: 'white', display: 'flex', alignItems: 'center' }}> 
-                    <FontAwesomeIcon icon={faUserCog} style={{ color: 'white' }} /> {name}
-                  </span>
-                }
-                id="navbarScrollingDropdownUser2"
-              >
-                <NavDropdown.Divider style={{ backgroundColor: 'white' }} />
-                <NavDropdown.Item style={{ color: 'white' }} onClick={desconectarse}>Desconectarse</NavDropdown.Item>
-              </NavDropdown>
-            )}
-            <li className="nav-item">
-              <Nav.Link title="Carro de compra" onClick={handleShowCarro}>
-                <FontAwesomeIcon icon={faCartShopping} />
-              </Nav.Link>
-            </li>
-          </ul>
+                <NavDropdown 
+                  title={
+                    <span style={{ color: 'white', display: 'flex', alignItems: 'center' }}> 
+                      <FontAwesomeIcon icon={faUserCog} style={{ color: 'white' }} /> {name}
+                    </span>
+                  }
+                  id="navbarScrollingDropdownUser2"
+                >
+                  <NavDropdown.Divider style={{ backgroundColor: 'white' }} />
+                  <NavDropdown.Item style={{ color: 'white' }} onClick={desconectarse}>Desconectarse</NavDropdown.Item>
+                </NavDropdown>
+              )}
+              <li className="nav-item">
+                <Nav.Link title="Carro de compra" onClick={handleShowCarro}>
+                  <FontAwesomeIcon icon={faCartShopping} />
+                </Nav.Link>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+
+      <Offcanvas show={showOffcanvas} onHide={handleCloseOffcanvas} placement="top" className="offcanvas-top">
+  <Offcanvas.Header closeButton>
+    <Offcanvas.Title>Productos</Offcanvas.Title>
+  </Offcanvas.Header>
+  <Offcanvas.Body>
+    <div className="product-list">
+      <div className="product">
+        <h5>Nintendo</h5>
+        <ul>
+          <li><a href="">Super Nintendo</a></li>
+          <li><a href="/nintendo-3ds">Wii</a></li>
+          <li><a href="/nintendo-wii">3DS</a></li>
+        </ul>
       </div>
-    </nav>
+      <div className="product">
+        <h5>PlayStation</h5>
+        <ul>
+          <li><a href="/ps1">PS2</a></li>
+          <li><a href="/ps2">PS3</a></li>
+          <li><a href="/ps4">PS4</a></li>
+        </ul>
+      </div>
+      <div className="product">
+        <h5>Xbox</h5>
+        <ul>
+          <li><a href="/xbox-series-x">Original Xbox</a></li>
+          <li><a href="/xbox360">Xbox 360</a></li>
+          <li><a href="/xbox-one">Xbox One</a></li>
+        </ul>
+      </div>
+      <div className="product">
+        <h5>Otros</h5>
+        <ul>
+          <li><a href="/ouya">Cables</a></li>
+          <li><a href="/atari">Cascos</a></li>
+          <li><a href="/commodore">Commodore</a></li>
+        </ul>
+      </div>
+    </div>
+  </Offcanvas.Body>
+</Offcanvas>
+    </>
   );
 };
 
---
+++ thedesbreact/src/productos/nintendo/supernintendo.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import Navbar from '../componentes/Navbar';
+import Footer from '../componentes/Footer';
+
+const supernintendo = () => {
+  return (
+    <div id="root">
+      <Navbar />
+      <div className="content d-flex align-items-center justify-content-center">
+        <Container fluid>
+          {/* Div de presentación */}
+          <div className="presentation text-center mb-4">
+            
+            <p>¡Aquí encontrarás todo lo que necesitas!</p>
+          </div>
+          {/* Una card grande con imagen a la derecha y texto a la izquierda */}
+          <Row className="justify-content-center mb-4">
+            <Col xl={8} md={12} className="mw-100">
+              <Card className="d-flex flex-row">
+                <Card.Body className="d-flex flex-column justify-content-center">
+                  <Card.Title>Producto Destacado</Card.Title>
+                  <Card.Text>
+                    Aquí puedes encontrar una descripción detallada del producto destacado. Explica por qué este producto es especial y cómo puede beneficiar a los clientes.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Img variant="right" src="ruta/a/imagen_destacada.jpg" className="w-50" />
+              </Card>
+            </Col>
+          </Row>
+          {/* Sección del formulario y explicación */}
+          <Row className="justify-content-center mb-4">
+            <Col xl={8} md={12} className="d-flex">
+              {/* Div explicativo */}
+              <Col md={6} className="d-flex align-items-center">
+                <div>
+                  <h2>Contáctenos</h2>
+                  <p>¿Necesita ayuda?<br />
+                     Llámenos, envíenos un correo electrónico o rellene el formulario. Nuestra línea telefónica de atención al cliente está abierta de 10 a. m. a 5 p. m. MST. Puede esperar una respuesta por correo electrónico dentro de 24 a 48 horas hábiles.<br /><br />
+                     <strong>Número de teléfono:</strong> +1 (877) 520-1219<br />
+                     <strong>Correo electrónico:</strong> Ayuda@thevideogamecompany.com
+                  </p>
+                </div>
+              </Col>
+              {/* Formulario de contacto */}
+              <Col md={6}>
+                <Form>
+                  <Form.Group controlId="formNombre">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control type="text" placeholder="Introduce tu nombre" />
+                  </Form.Group>
+                  <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Introduce tu email" />
+                  </Form.Group>
+                  <Form.Group controlId="formMensaje">
+                    <Form.Label>Mensaje</Form.Label>
+                    <Form.Control as="textarea" rows={3} placeholder="Introduce tu mensaje" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Enviar
+                  </Button>
+                </Form>
+              </Col>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default supernintendo;
